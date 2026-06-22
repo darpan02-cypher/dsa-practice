a9bda06bcbd1c7d5ee5de1cd1e7a64363346a2ff
Himanshi Shrivas <hshriva1@charlotte.edu>
2026-06-22 02:35:28 +0000

    house robber II - my solution with twp dp arrays without helper function but it has logic redundancy that violates DRY appraoch in coding

--- dsa-patterns/Dynamic Programming/houseRoberII.py
+++ dsa-patterns/Dynamic Programming/houseRoberII.py
@@ -1,6 +1,10 @@
 from typing import List
 
 class Solution:
+
+#standard solution using helper function to handle the linear case of the house robber problem--
+
+    '''
     def rob(self, nums: List[int]) -> int:
         if len(nums) == 1: return nums[0]
         
@@ -14,6 +18,35 @@ class Solution:
         
         # Split into the two circular scenarios
         return max(rob_linear(nums[:-1]), rob_linear(nums[1:]))
+    '''
+
+
+# my solution 
+
+    def rob(self, nums: List[int]) -> int:
+        # 1. Move bse cases to top
+        if len(nums) == 0: return 0
+        if len(nums) == 1: return nums[0]
+        if len(nums) == 2: return max(nums[0], nums[1])
+
+        # scenerio 1- rob first house and skip last house
+        dp1 = [0] * len(nums)
+        dp1[0] = nums[0]
+        dp1[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums) - 1):
+            dp1[i] = max(dp1[i - 1], nums[i] + dp1[i - 2]) # same house robber logic as in house robber I problem
+
+        result1 = dp1[len(nums) - 2] # last house is skipped
+
+        # scenerio 2- skip first house and rob last house
+        dp2 = [0] * len(nums)
+        dp2[1] = nums[1]
+        dp2[2] = max(nums[1], nums[2])
+        for i in range(3, len(nums)):
+            dp2[i] = max(dp2[i - 1], nums[i] + dp2[i - 2]) # same house robber logic as in house robber I problem
+        result2 = dp2[len(nums) - 1] # first house is skipped
+
+        return max(result1, result2) # return the maximum of the two scenarios
     
 # Example usage:
 solution = Solution()
