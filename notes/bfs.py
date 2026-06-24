bbcfd6fd8974368a6cf878037940b6b12bb8955e
Himanshi Shrivas <hshriva1@charlotte.edu>
2026-06-24 01:42:06 +0000

    notes to bfs approach

--- notes/bfs.py
+++ notes/bfs.py
@@ -1,10 +1,10 @@
 def bfs(graph, start_node):
-    visited = set()
-    queue = [start_node]
+    visited = set()  # why set - it does not cunt duplicates then? - Because a set automatically handles uniqueness, ensuring each node is visited only once.
+    queue = [start_node]  # queue - as a list to store nodes to be visited, (In python we use a list as a queue, unless we use collections.deque)
     visited.add(start_node)
 
     while queue:
-        current_node = queue.pop(0)
+        current_node = queue.pop(0) # Dequeue a node from the front of the queue - this will process nodes in a breadth-first manner
         print(current_node)
 
         for neighbor in graph[current_node]:
@@ -37,3 +37,14 @@ if __name__ == "__main__":
         graph[v].append(u)
 
     bfs(graph, 'A')
+
+
+#in simple bfs template to remember lifelong -
+# 1. Initialize visited set and queue - (You need to keep track of visited nodes to avoid cycles and a queue to explore nodes level by level. )
+# 2. Start with the initial node - Add the starting node to the queue and mark it as visited.
+# 3. While the queue is not empty - Continue exploring until there are no more nodes to visit.
+# 4. Dequeue a node - Remove the front node from the queue and process it (e.g., print it).
+# 5. Explore neighbors - For each neighbor of the current node, if it hasn't been visited, mark it as visited and add it to the queue for future exploration.
+# 6. Repeat - Go back to step 3 until all reachable nodes have been visited.
+
+# Link video - https://www.youtube.com/watch?v=pcKY4hjDrxk&t=246s 
